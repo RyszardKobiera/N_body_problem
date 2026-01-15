2aa5b61389e95f39e48b23abcfb554025e34d312
RyszardKobiera <rysio.kobiera@gmail.com>
2026-01-15 15:43:01 +0100

    adding bigger files

---
+++ build/CMakeFiles/Project.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/rysiek/Documents/1-kopia/ImGUI_custom_bundle/backends/imgui_impl_glfw.cpp" "CMakeFiles/Project.dir/ImGUI_custom_bundle/backends/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/Project.dir/ImGUI_custom_bundle/backends/imgui_impl_glfw.cpp.o.d"
+  "/home/rysiek/Documents/1-kopia/ImGUI_custom_bundle/backends/imgui_impl_opengl3.cpp" "CMakeFiles/Project.dir/ImGUI_custom_bundle/backends/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/Project.dir/ImGUI_custom_bundle/backends/imgui_impl_opengl3.cpp.o.d"
+  "/home/rysiek/Documents/1-kopia/ImGUI_custom_bundle/context_setup/context_setup.cpp" "CMakeFiles/Project.dir/ImGUI_custom_bundle/context_setup/context_setup.cpp.o" "gcc" "CMakeFiles/Project.dir/ImGUI_custom_bundle/context_setup/context_setup.cpp.o.d"
+  "/home/rysiek/Documents/1-kopia/ImGUI_custom_bundle/imgui.cpp" "CMakeFiles/Project.dir/ImGUI_custom_bundle/imgui.cpp.o" "gcc" "CMakeFiles/Project.dir/ImGUI_custom_bundle/imgui.cpp.o.d"
+  "/home/rysiek/Documents/1-kopia/ImGUI_custom_bundle/imgui_draw.cpp" "CMakeFiles/Project.dir/ImGUI_custom_bundle/imgui_draw.cpp.o" "gcc" "CMakeFiles/Project.dir/ImGUI_custom_bundle/imgui_draw.cpp.o.d"
+  "/home/rysiek/Documents/1-kopia/ImGUI_custom_bundle/imgui_tables.cpp" "CMakeFiles/Project.dir/ImGUI_custom_bundle/imgui_tables.cpp.o" "gcc" "CMakeFiles/Project.dir/ImGUI_custom_bundle/imgui_tables.cpp.o.d"
+  "/home/rysiek/Documents/1-kopia/ImGUI_custom_bundle/imgui_widgets.cpp" "CMakeFiles/Project.dir/ImGUI_custom_bundle/imgui_widgets.cpp.o" "gcc" "CMakeFiles/Project.dir/ImGUI_custom_bundle/imgui_widgets.cpp.o.d"
+  "/home/rysiek/Documents/1-kopia/ImGUI_custom_bundle/implot.cpp" "CMakeFiles/Project.dir/ImGUI_custom_bundle/implot.cpp.o" "gcc" "CMakeFiles/Project.dir/ImGUI_custom_bundle/implot.cpp.o.d"
+  "/home/rysiek/Documents/1-kopia/ImGUI_custom_bundle/implot_items.cpp" "CMakeFiles/Project.dir/ImGUI_custom_bundle/implot_items.cpp.o" "gcc" "CMakeFiles/Project.dir/ImGUI_custom_bundle/implot_items.cpp.o.d"
+  "/home/rysiek/Documents/1-kopia/main.cpp" "CMakeFiles/Project.dir/main.cpp.o" "gcc" "CMakeFiles/Project.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Project.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project.dir/ImGUI_custom_bundle/backends/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/Project.dir/ImGUI_custom_bundle/backends/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/Project.dir/ImGUI_custom_bundle/backends/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/Project.dir/ImGUI_custom_bundle/backends/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/Project.dir/ImGUI_custom_bundle/context_setup/context_setup.cpp.o"
+  "CMakeFiles/Project.dir/ImGUI_custom_bundle/context_setup/context_setup.cpp.o.d"
+  "CMakeFiles/Project.dir/ImGUI_custom_bundle/imgui.cpp.o"
+  "CMakeFiles/Project.dir/ImGUI_custom_bundle/imgui.cpp.o.d"
+  "CMakeFiles/Project.dir/ImGUI_custom_bundle/imgui_draw.cpp.o"
+  "CMakeFiles/Project.dir/ImGUI_custom_bundle/imgui_draw.cpp.o.d"
+  "CMakeFiles/Project.dir/ImGUI_custom_bundle/imgui_tables.cpp.o"
+  "CMakeFiles/Project.dir/ImGUI_custom_bundle/imgui_tables.cpp.o.d"
+  "CMakeFiles/Project.dir/ImGUI_custom_bundle/imgui_widgets.cpp.o"
+  "CMakeFiles/Project.dir/ImGUI_custom_bundle/imgui_widgets.cpp.o.d"
+  "CMakeFiles/Project.dir/ImGUI_custom_bundle/implot.cpp.o"
+  "CMakeFiles/Project.dir/ImGUI_custom_bundle/implot.cpp.o.d"
+  "CMakeFiles/Project.dir/ImGUI_custom_bundle/implot_items.cpp.o"
+  "CMakeFiles/Project.dir/ImGUI_custom_bundle/implot_items.cpp.o.d"
+  "CMakeFiles/Project.dir/main.cpp.o"
+  "CMakeFiles/Project.dir/main.cpp.o.d"
+  "Project"
+  "Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
